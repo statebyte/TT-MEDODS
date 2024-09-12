6d94c25b8a77f2bd3cf2426334d818038695b530
FIVE <elsaudomar@gmail.com>
2024-09-12 14:21:31 +0300

    Add more tests

--- src/tests/jwt_test.go
+++ src/tests/jwt_test.go
@@ -21,16 +21,37 @@ func TestGenerateToken(t *testing.T) {
 		t.Fatalf("expected valid token, got error %v", err)
 	}
 
-	payload, err := jwtGen.VerifyToken(tokens.Pair.AccessToken, true)
+	payloadAccess, err := jwtGen.VerifyToken(tokens.Pair.AccessToken, true)
 	if err != nil {
-		t.Fatalf("expected valid token, got error %v", err)
+		t.Fatalf("expected valid access token, got error %v", err)
 	}
 
-	if payload.IPAddress != ip {
-		t.Fatalf("expected ip adress, got error %v", err)
+	if payloadAccess.TokenUUID != tokens.TokenUUID {
+		t.Fatalf("expected access token UUID, got error %v", err)
 	}
 
-	if payload.UserID != userID {
-		t.Fatalf("expected ip adress, got error %v", err)
+	if payloadAccess.IPAddress != ip {
+		t.Fatalf("expected access token ip adress, got error %v", err)
+	}
+
+	if payloadAccess.UserID != userID {
+		t.Fatalf("expected access token ip adress, got error %v", err)
+	}
+
+	payloadRefresh, err := jwtGen.VerifyToken(tokens.Pair.RefreshToken, false)
+	if err != nil {
+		t.Fatalf("expected valid refresh token, got error %v", err)
+	}
+
+	if payloadRefresh.TokenUUID != tokens.TokenUUID {
+		t.Fatalf("expected refresh token UUID, got error %v", err)
+	}
+
+	if payloadRefresh.IPAddress != ip {
+		t.Fatalf("expected refresh token ip adress, got error %v", err)
+	}
+
+	if payloadRefresh.UserID != userID {
+		t.Fatalf("expected refresh token ip adress, got error %v", err)
 	}
 }
